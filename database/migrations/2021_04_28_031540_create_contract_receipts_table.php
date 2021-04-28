3691105a0d54f9124a88506033dccc7dca58dd15
Rifqi14 <rifqi.persie@gmail.com>
2021-04-28 12:07:10 +0700

    Add foreign to contract_receipts table

--- database/migrations/2021_04_28_031540_create_contract_receipts_table.php
+++ database/migrations/2021_04_28_031540_create_contract_receipts_table.php
@@ -16,11 +16,15 @@ class CreateContractReceiptsTable extends Migration
         Schema::create('contract_receipts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('contract_id');
+            $table->unsignedBigInteger('warehouse_id');
             $table->integer('batch');
             $table->integer('total_batch');
             $table->text('remarks')->nullable();
             $table->string('status');
             $table->timestamps();
+
+            $table->foreign('contract_id')->references('id')->on('contracts')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
