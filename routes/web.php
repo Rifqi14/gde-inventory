934b7ff6612d490d4a893566bf2ad9355f71e630
Franata <rosandi.mahardika@gmail.com>
2021-06-28 13:49:35 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,7 @@ Route::group(['prefix' => 'admin'], function () {
         Route::resource('/purchasing', 'Admin\PurchasingController');
         // Route Stock Adjustment
         Route::get('/stockadjustment/read', 'Admin\StockAdjustmentController@read')->name('stockadjustment.read');
+        Route::get('/stockadjustment/selectproduct', 'Admin\StockAdjustmentController@selectproduct')->name('stockadjustment.selectproduct');
         Route::resource('/stockadjustment', 'Admin\StockAdjustmentController');
         // Route Warehouse
         Route::get('/warehouse/read', 'Admin\WarehouseController@read')->name('warehouse.read');
