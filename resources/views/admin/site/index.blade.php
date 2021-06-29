0b5a1b18f8decf1a94ab0afb8b32ab37902326ff
palupigalih81 <palupigalih81@gmail.com>
2021-06-29 11:24:57 +0700

    Setup  warehouse virtual on site and role access user

--- resources/views/admin/site/index.blade.php
+++ resources/views/admin/site/index.blade.php
@@ -1,16 +1,24 @@
 @extends('admin.layouts.app')
-@section('title', 'Site')
+@section('title', $menu_name)
 @section('stylesheets')
 
 @endsection
 
 @section('button')
-<button type="button" id="add-role" class="btn btn-labeled text-sm btn-sm btn-success btn-flat legitRipple" onclick="windowLocation('{{route('site.create')}}')">
-    <b><i class="fas fa-plus"></i></b> Create
+@if (in_array('create', $actionmenu))
+<button type="button" class="btn btn-labeled text-sm btn-sm btn-success btn-flat legitRipple" onclick="windowLocation('{{ route('site.create') }}')">
+    <b>
+        <i class="fas fa-plus"></i>
+    </b> Create
 </button>
-<button type="button" id="filter-role" class="btn btn-labeled text-sm btn-sm btn-default btn-flat legitRipple" onclick="filter()">
-    <b><i class="fas fa-search"></i></b> Filter
+@endif
+@if (in_array('read', $actionmenu))
+<button type="button" class="btn btn-labeled text-sm btn-sm btn-default btn-flat legitRipple" onclick="filter()">
+    <b>
+        <i class="fas fa-search"></i>
+    </b> Search
 </button>
+@endif
 @endsection
 
 @section('breadcrumb')
@@ -22,8 +30,8 @@
     </div>
     <div class="col-sm-8">
         <ol class="breadcrumb float-sm-right text-danger mr-2 text-sm">
-            <li class="breadcrumb-item">Home</li>
-            <li class="breadcrumb-item">Site</li>
+            <li class="breadcrumb-item">{{$parent_name}}</li>
+            <li class="breadcrumb-item">{{$menu_name}}</li>
         </ol>
     </div>
 </div>
@@ -36,18 +44,18 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        
+
                     </div>
                     <div class="card-body table-responsive p-0">
                         <table id="table-site" class="table table-striped datatable" width="100%">
                             <thead>
                                 <tr>
-                                    <th width="5">No.</th>
+                                    <th width="5" class="text-center">No.</th>
                                     <th width="100">Code</th>
                                     <th width="100">Site Name</th>
                                     <th width="20" class="text-center">Created at</th>
                                     <th width="20" class="text-center">Updated at</th>
-                                    <th width="20" class="text-center">#</th>
+                                    <th width="20" class="text-center">Action</th>
                                 </tr>
                             </thead>
                         </table>
@@ -62,9 +70,9 @@
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title text-bold">Filter Role</h5>
+                <h5 class="modal-title">Filter {{$menu_name}}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
@@ -81,8 +89,7 @@
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label class="control-label" for="name">Name</label>
-                                <input type="text" name="name" class="form-control"
-                                    placeholder="Name">
+                                <input type="text" name="name" class="form-control" placeholder="Name">
                             </div>
                         </div>
                     </div>
@@ -103,102 +110,141 @@
 
 @section('scripts')
 <script>
-function filter() {
-    $('#form-filter').modal('show');
-}
-function edit(id) {
-    document.location = '{{route('site.index')}}/' + id + '/edit';
-}
-function view(id) {
-    document.location = '{{route('site.index')}}/' + id;
-}
-$(function(){
-    dataTable = $('.datatable').DataTable( {
-        processing: true,
-        language: {
-            processing: `<div class="p-2 text-center">
+    var actionmenu = JSON.parse('{!! json_encode($actionmenu) !!}');
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    $(function() {
+        dataTable = $('.datatable').DataTable({
+            processing: true,
+            language: {
+                processing: `<div class="p-2 text-center">
           <i class="fas fa-circle-notch fa-spin fa-fw"></i> Loading...
         </div>`
-        },
-        serverSide: true,
-        filter: false,
-        responsive: true,
-        lengthChange: false,
-        order: [[ 1, "asc" ]],
-        ajax: {
-            url: "{{route('site.read')}}",
-            type: "GET",
-            data:function(data){
-                var code = $('#form-search').find('input[name=code]').val();
-                var name = $('#form-search').find('input[name=name]').val();
-                data.code = code;
-                data.name = name;
-            }
-        },
-        columnDefs:[
-            {
-                orderable: false,targets:[0,5]
             },
-            { className: "text-right", targets: [0] },
-            { className: "text-center", targets: [3,4,5] },
-            {
-                width: "5%",
-                render: function ( data, type, row ) {
-                    return row.no;
-                },targets: [0]
+            serverSide: true,
+            filter: false,
+            responsive: true,
+            lengthChange: false,
+            order: [
+                [1, "asc"]
+            ],
+            ajax: {
+                url: "{{route('site.read')}}",
+                type: "GET",
+                data: function(data) {
+                    var code = $('#form-search').find('input[name=code]').val();
+                    var name = $('#form-search').find('input[name=name]').val();
+                    data.code = code;
+                    data.name = name;
+                }
             },
-            { 
-                width: "15%",
-                targets: [1]
-            },
-            { 
-                width: "15%",
-                targets: [3]
-            },
-            { 
-                width: "15%",
-                targets: [4]
-            },
-            {   
-                width: "10%",
-                render: function ( data, type, row ) {
-                return `<div class="btn-group">
-                  <button type="button" class="btn btn-flat btn-sm dropdown-toggle" data-toggle="dropdown">
-                    <i class="fas fa-bars"></i>
-                  </button>
-                  <div class="dropdown-menu">
-                    <a class="dropdown-item" href="javascript:void(0);" onclick="view(${row.id})">
-                      <i class="far fa-eye"></i>View Data
-                    </a>
-                    <a class="dropdown-item" href="javascript:void(0);" onclick="edit(${row.id})">
-                      <i class="far fa-edit"></i>Update Data
-                    </a>
-                    <a class="dropdown-item delete" href="javascript:void(0);" data-id="${row.id}">
-                      <i class="fa fa-trash-alt"></i> Delete Data
-                    </a>
-                  </div>
-                </div>`;
-                },targets: [5]
-            }
-        ],
-        columns: [
-            { data: "no" },
-            { data: "code" },
-            { data: "name" },
-            { data: "created_at" },
-            { data: "updated_at" },
-            { data: "no" },
-        ]
+            columnDefs: [{
+                    orderable: false,
+                    targets: [0, 5]
+                },
+                {
+                    className: "text-right",
+                    targets: []
+                },
+                {
+                    className: "text-center",
+                    targets: [0, 3, 4, 5]
+                },
+                {
+                    width: "5%",
+                    render: function(data, type, row) {
+                        return row.no;
+                    },
+                    targets: [0]
+                },
+                {
+                    width: "15%",
+                    targets: [1]
+                },
+                {
+                    width: "15%",
+                    targets: [3]
+                },
+                {
+                    width: "15%",
+                    targets: [4]
+                },
+                {
+                    width: "10%",
+                    render: function(data, type, row) {
+                        var button = '';
+                        button += `<a class="dropdown-item" href="javascript:void(0);" onclick="detail(${row.id})">
+                                        <i class="far fa-eye"></i>View Data
+                                    </a>`;
+                        // update
+                        if (actionmenu.indexOf('update') > 0) {
+                            button += `<a class="dropdown-item" href="javascript:void(0);" onclick="edit(${row.id})">
+                                        <i class="far fa-edit"></i>Update Data
+                                    </a>`;
+                        }
+                        // delete
+                        if (actionmenu.indexOf('delete') > 0) {
+                            button += `<a class="dropdown-item" href="javascript:void(0);" onclick="destroy(${row.id})">
+                                        <i class="fa fa-trash-alt"></i> Delete Data
+                                    </a>`;
+                        }
+                        return `<div class="btn-group">
+                        <button type="button" class="btn btn-flat btn-sm dropdown-toggle" data-toggle="dropdown">
+                            <i class="fas fa-bars"></i>
+                        </button>
+                        <div class="dropdown-menu">
+                            ${button}
+                        </div>
+                    </div>`;
+                    },
+                    targets: [5]
+                }
+            ],
+            columns: [{
+                    data: "no"
+                },
+                {
+                    data: "code"
+                },
+                {
+                    data: "name"
+                },
+                {
+                    data: "created_at"
+                },
+                {
+                    data: "updated_at"
+                },
+                {
+                    data: "no"
+                },
+            ]
+        });
+
+        $('#form-search').submit(function(e) {
+            e.preventDefault();
+            dataTable.draw();
+            $('#form-filter').modal('hide');
+        });
     });
 
-    $('#form-search').submit(function (e) {
-        e.preventDefault();
-        dataTable.draw();
-        $('#form-filter').modal('hide');
-    })
-
-    $(document).on('click', '.delete', function () {
-        var id = $(this).data('id');
+    const destroy = (id) => {
         bootbox.confirm({
             buttons: {
                 confirm: {
@@ -212,7 +258,7 @@ $(function(){
             },
             title: 'Delete data?',
             message: 'Are you sure want to delete this site?',
-            callback: function (result) {
+            callback: function(result) {
                 if (result) {
                     var data = {
                         _token: "{{ csrf_token() }}"
@@ -222,77 +268,37 @@ $(function(){
                         dataType: 'json',
                         data: data,
                         type: 'DELETE',
-                        beforeSend: function () {
+                        beforeSend: function() {
                             blockMessage('#content', 'Loading', '#fff');
                         }
-                    }).done(function (response) {
+                    }).done(function(response) {
                         $('#content').unblock();
                         if (response.status) {
-                            toastr.options = {
-                                "closeButton": false,
-                                "debug": false,
-                                "newestOnTop": false,
-                                "progressBar": false,
-                                "positionClass": "toast-top-right",
-                                "preventDuplicates": false,
-                                "onclick": null,
-                                "showDuration": "300",
-                                "hideDuration": "1000",
-                                "timeOut": "5000",
-                                "extendedTimeOut": "1000",
-                                "showEasing": "swing",
-                                "hideEasing": "linear",
-                                "showMethod": "fadeIn",
-                                "hideMethod": "fadeOut"
-                            }
                             toastr.success(response.message);
                             dataTable.ajax.reload(null, false);
-                        }else {
-                            toastr.options = {
-                                "closeButton": false,
-                                "debug": false,
-                                "newestOnTop": false,
-                                "progressBar": false,
-                                "positionClass": "toast-top-right",
-                                "preventDuplicates": false,
-                                "onclick": null,
-                                "showDuration": "300",
-                                "hideDuration": "1000",
-                                "timeOut": "5000",
-                                "extendedTimeOut": "1000",
-                                "showEasing": "swing",
-                                "hideEasing": "linear",
-                                "showMethod": "fadeIn",
-                                "hideMethod": "fadeOut"
-                            }
+                        } else {
                             toastr.warning(response.message);
                         }
-                    }).fail(function (response) {
+                    }).fail(function(response) {
                         var response = response.responseJSON;
                         $('#content').unblock();
-                        toastr.options = {
-                            "closeButton": false,
-                            "debug": false,
-                            "newestOnTop": false,
-                            "progressBar": false,
-                            "positionClass": "toast-top-right",
-                            "preventDuplicates": false,
-                            "onclick": null,
-                            "showDuration": "300",
-                            "hideDuration": "1000",
-                            "timeOut": "5000",
-                            "extendedTimeOut": "1000",
-                            "showEasing": "swing",
-                            "hideEasing": "linear",
-                            "showMethod": "fadeIn",
-                            "hideMethod": "fadeOut"
-                        }
                         toastr.warning(response.message);
                     })
                 }
             }
         });
-    })
-});
+    }
+
+    function filter() {
+        $('#form-filter').modal('show');
+    }
+
+    function edit(id) {
+        document.location = `{{route('site.index')}}/${id}/edit`;
+    }
+
+    function view(id) {
+        document.location = `{{route('site.index')}}/${id}`;
+    }
 </script>
 @endsection
